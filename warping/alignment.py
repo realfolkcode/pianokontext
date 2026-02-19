7635689c6e0dce471a09d6f33f9c1cf3f086d2dd
realfolkcode <wtfomgrofl@mail.ru>
2026-02-19 12:12:54 +0300

    Add DTW path calculation util

--- warping/alignment.py
+++ warping/alignment.py
@@ -1,4 +1,7 @@
 import numpy as np
+import torch
+from dtaidistance import dtw_ndim, ed
+from typing import Dict
 
 
 def cut_embedding(emb,
@@ -25,3 +28,39 @@ def cut_embedding(emb,
         emb = emb[start_idx:]
     
     return emb
+
+
+def calculate_dtw_path(deadpan_emb: torch.Tensor,
+                       expressive_emb: torch.Tensor,
+                       window: int = 768) -> Dict:
+    """Calculates the DTW path between the embeddings.
+
+    Args:
+        deadpan_emb: Deadpan embedding of shape (N, D).
+        expressive_emb: Expressive embedding of shape (N, D).
+        window: Sakoe-Chiba band in frames.
+    
+    Returns:
+        A dictionary with keys "deadpan" and "expressive" containing
+        the corresponding embedding indices.
+    """
+    deadpan_emb = deadpan_emb.numpy()
+    deadpan_emb = deadpan_emb.astype(np.double)
+
+    expressive_emb = expressive_emb.numpy()
+    expressive_emb = expressive_emb.astype(np.double)
+
+    max_dist = ed.distance(deadpan_emb,
+                           expressive_emb,
+                           use_ndim=True)
+
+    dtw_path = dtw_ndim.warping_path(deadpan_emb,
+                                     expressive_emb,
+                                     window=window,
+                                     max_dist=max_dist)
+    dtw_path = torch.tensor(dtw_path, dtype=int)
+
+    dtw_path_dict = {"deadpan": dtw_path[:, 0],
+                     "expressive": dtw_path[:, 1]}
+
+    return dtw_path_dict
